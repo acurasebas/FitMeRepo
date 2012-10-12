08aae002a850c24519b6644822053938c114188b
Álvaro García <alvaroastur4@gmail.com>
2012-10-11 22:45:31 -0400

    Intents 3 primeras ventanas

--- FitMe/gen/com/fitmetracker/fitme/R.java
+++ FitMe/gen/com/fitmetracker/fitme/R.java
@@ -927,7 +927,7 @@ containing a value of this type.
         public static final int logolog=0x7f020072;
     }
     public static final class id {
-        public static final int Middle=0x7f040034;
+        public static final int MiddleLogin=0x7f040034;
         public static final int Texto=0x7f040037;
         public static final int abs__action_bar=0x7f040022;
         public static final int abs__action_bar_container=0x7f040021;
@@ -969,14 +969,16 @@ containing a value of this type.
         public static final int abs__title=0x7f04001b;
         public static final int abs__titleDivider=0x7f04001c;
         public static final int abs__up=0x7f04000b;
-        public static final int btnLogin=0x7f040035;
-        public static final int button1=0x7f040036;
+        public static final int btnLoginLogin=0x7f040035;
+        public static final int btnRegisterLogin=0x7f040036;
+        public static final int btnRegisterRegister=0x7f040039;
         public static final int disableHome=0x7f040009;
         public static final int edit_query=0x7f040027;
-        public static final int header=0x7f040033;
+        public static final int headerLogin=0x7f040033;
+        public static final int headerRegister=0x7f040038;
         public static final int homeAsUp=0x7f040006;
         public static final int listMode=0x7f040002;
-        public static final int menu_settings=0x7f040038;
+        public static final int menu_settings=0x7f04003a;
         public static final int normal=0x7f040001;
         public static final int showCustom=0x7f040008;
         public static final int showHome=0x7f040005;
--- FitMe/src/com/fitmetracker/fitme/LoginActivity.java
+++ FitMe/src/com/fitmetracker/fitme/LoginActivity.java
@@ -2,14 +2,47 @@ package com.fitmetracker.fitme;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        
+        Button buttonCreateNewAccount = (Button) findViewById(R.id.btnRegisterLogin);
+        
+        // Listening to the Register Button
+        
+        buttonCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// Switching to the Register Screen
+				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+				startActivity(i);
+				
+			}
+		});
+    
+       Button buttonLogin = (Button) findViewById(R.id.btnLoginLogin);
+       
+       // Listenting to the LogIn Button
+       
+       buttonLogin.setOnClickListener(new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			// Swtiching to the Main View
+			Intent i = new Intent(getApplicationContext(), MainScreenActivity.class);
+			startActivity(i);
+			
+		}
+	});
+        
     }
 
     @Override
