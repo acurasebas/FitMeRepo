ce18e30c1644d1065a8e394fd394e0df518b8342
Álvaro García <alvaroastur4@gmail.com>
2012-10-11 21:49:49 -0400

    Rediseño de login y register

--- FitMe/gen/com/actionbarsherlock/R.java
+++ FitMe/gen/com/actionbarsherlock/R.java
@@ -926,7 +926,8 @@ containing a value of this type.
         public static final int logolog=0x7f020072;
     }
     public static final class id {
-        public static final int Texto=0x7f040036;
+        public static final int Middle=0x7f040034;
+        public static final int Texto=0x7f040037;
         public static final int abs__action_bar=0x7f040022;
         public static final int abs__action_bar_container=0x7f040021;
         public static final int abs__action_bar_subtitle=0x7f040011;
@@ -967,14 +968,14 @@ containing a value of this type.
         public static final int abs__title=0x7f04001b;
         public static final int abs__titleDivider=0x7f04001c;
         public static final int abs__up=0x7f04000b;
-        public static final int btnLogin=0x7f040034;
-        public static final int btnRegister=0x7f040035;
+        public static final int btnLogin=0x7f040035;
+        public static final int button1=0x7f040036;
         public static final int disableHome=0x7f040009;
         public static final int edit_query=0x7f040027;
         public static final int header=0x7f040033;
         public static final int homeAsUp=0x7f040006;
         public static final int listMode=0x7f040002;
-        public static final int menu_settings=0x7f040037;
+        public static final int menu_settings=0x7f040038;
         public static final int normal=0x7f040001;
         public static final int showCustom=0x7f040008;
         public static final int showHome=0x7f040005;
